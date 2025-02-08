d7518e68ea36872738374be2c615abb09f480cb5
JJ <35785259+jj10133@users.noreply.github.com>
2025-02-08 17:43:43 +0000

    Update MainActivity.kt

--- app/src/main/java/to/holepunch/bare/android/MainActivity.kt
+++ app/src/main/java/to/holepunch/bare/android/MainActivity.kt
@@ -2,6 +2,7 @@ package to.holepunch.bare.android
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
